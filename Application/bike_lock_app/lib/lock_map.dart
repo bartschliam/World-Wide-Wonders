b2cf3091facb5a212d2868c0a0b2064355a6c086
samhartshorn <hartshos@tcd.ie>
2023-02-21 22:22:26 +0000

    Map on the app

--- Application/bike_lock_app/lib/lock_map.dart
+++ Application/bike_lock_app/lib/lock_map.dart
@@ -1,15 +1,64 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// class LockMap extends StatefulWidget {
+//   const LockMap({super.key});
+//
+//   @override
+//   State<LockMap> createState() => _LockMapState();
+// }
+//
+// class _LockMapState extends State<LockMap> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Image.asset('assets/images/ui_map.JPG');
+//   }
+// }
 
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+//Create a Google Maps widget.
 class LockMap extends StatefulWidget {
-  const LockMap({super.key});
+  const LockMap({Key? key}) : super(key: key);
 
   @override
-  State<LockMap> createState() => _LockMapState();
+  State<LockMap> createState() => LockMapState();
 }
 
-class _LockMapState extends State<LockMap> {
+class LockMapState extends State<LockMap> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition dublin = CameraPosition(
+    target: LatLng(53.3458, -6.2543577),
+    zoom: 14,
+  );
+
+  static const CameraPosition trinity = CameraPosition(
+      bearing: 50, target: LatLng(53.3447406, -6.2584452), tilt: 40, zoom: 18);
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/images/ui_map.JPG');
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: dublin,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToCollege,
+        label: const Text('To College!'),
+        icon: const Icon(Icons.school),
+      ),
+    );
+  }
+
+  Future<void> _goToCollege() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(trinity));
   }
 }
