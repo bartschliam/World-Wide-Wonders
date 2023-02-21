06ef8cd415f03ef132761c2034ab0f3cbffe846b
Liam Bartsch <bartschl@tcd.ie>
2023-02-21 09:14:02 +0000

    added time

--- board/ESP32_Firebase/ESP32_Firebase.ino
+++ board/ESP32_Firebase/ESP32_Firebase.ino
@@ -1,5 +1,4 @@
 #include <FirebaseESP32.h>
-
 #include <Arduino.h>
 #define LED 2
 
@@ -11,7 +10,10 @@
 // #define WIFI_PASSWORD "password?"
 #define API_KEY "pzmetHjgzVn2I3lSQoevlBWGxZb7eR4h9dfVgGGi"
 #define DATABASE_URL "https://iot-bike-lock-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
 FirebaseData firebaseData;
+char cr[] = __DATE__;
+char ct[] = __TIME__;
 
 void control_led() {
   if(Firebase.getBool(firebaseData, "/locks/lock0/locked")){
@@ -24,7 +26,7 @@ void control_led() {
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(115200);
-  Serial.print("[2J");
+  Serial.print(cr, ct);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
