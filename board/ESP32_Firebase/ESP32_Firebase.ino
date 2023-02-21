6367c661e15c9ee61b208bb72aae3b251db0dd3a
Liam Bartsch <bartschl@tcd.ie>
2023-02-21 15:53:21 +0000

    working led not working bluetooth

--- board/ESP32_Firebase/ESP32_Firebase.ino
+++ board/ESP32_Firebase/ESP32_Firebase.ino
@@ -38,6 +38,30 @@ void control_led() {
   }
 }
 
+void updateSerial(){
+  delay(500);
+  while (Serial.available())  {
+    Serial2.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while (Serial2.available())  {
+    Serial.write(Serial2.read());//Forward what Software Serial received to Serial Port
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid()){
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+}
+
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(115200);
@@ -60,16 +84,21 @@ void setup() {
   
   //SerialBT.begin("ESP32 Bike Lock");
   Serial.println("Bluetooth Started! Ready to pair...");
+
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  delay(3000);
 }
 
 void loop() {
   control_led();
-  Serial.print(gps.location.lat(), 6);
-  Serial.print(F(","));
-  Serial.println(gps.location.lng(), 6);
-  if (gps.location.isValid()){
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
+  updateSerial();
+  while (Serial2.available() > 0)
+    if (gps.encode(Serial2.read()))
+      displayInfo();
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
   }
 }
